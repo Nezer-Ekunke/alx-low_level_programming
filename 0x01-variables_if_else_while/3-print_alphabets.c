4c9d45216d1c779a0f5ad116a93a31abd1995f8f
Nezer-Ekunke <nezerekunke@gmail.com>
2023-02-20 20:57:05 -0800

    If, Else & While

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the alpahbet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[1]);
+	}
+	putchar('\n');
+	return (0);
+}
